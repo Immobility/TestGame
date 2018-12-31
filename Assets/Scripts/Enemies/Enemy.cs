4cee948f0fcfdbb576c7eeda695363e944bbb360
Dong Jun Kim <dongjunkim13@gmail.com>
2018-12-31 01:14:05 -0500

    Added an enemy
    Added an enemy called (Logg) that follows you. If ran away to a certain distance, it would be put in Idle stance.

--- Assets/Scripts/Enemies/Enemy.cs
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -2,18 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum EnemyState
+{
+    idle, idle2, attack, die
+}
+
 public class Enemy : MonoBehaviour
 {
-
+    public EnemyState currentState;
     public int health;
     public string enemyName;
     public int baseAttack;
     public float moveSpeed;
+    public Animator animator;
 
 	// Use this for initialization
 	void Start()
     {
-		
+
 	}
 	
 	// Update is called once per frame
--- Assets/Scripts/Enemies/log.cs
+++ Assets/Scripts/Enemies/log.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Log : Enemy
 {
     public Transform target;
@@ -9,15 +10,44 @@ public class Log : Enemy
     public float chaseRadius;
     public float attackRadius;
 
-	// Use this for initialization
-	void Start()
+    // Use this for initialization
+    void Start()
     {
-		
+        animator = GetComponent<Animator>();
+        target = GameObject.FindWithTag("Player").transform;
 	}
 	
 	// Update is called once per frame
 	void Update()
     {
-		
+        isAwake();
+        CheckDistance();
+        
 	}
+
+    void CheckDistance()
+    {
+        if (Vector3.Distance(target.position, transform.position) <= chaseRadius)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
+            animator.SetBool("seePlayer", true);
+            currentState = EnemyState.attack; // Also sets Attack bool to true
+        }
+        else
+        {
+            currentState = EnemyState.idle2;
+            animator.SetBool("seePlayer", false);
+        }
+    }
+
+    void isAwake()
+    {
+        if (Vector3.Distance(target.position, transform.position) <= 6 )
+        {
+            animator.SetBool("isWake", true);
+            currentState = EnemyState.idle2;
+        }
+    }
+
+
 }
